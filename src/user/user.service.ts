4a5d14576cd8d4945eb43211aff182c72ce358f6
HoaiBaoNguyen <hoaibao2129@gmail.com>
2024-08-09 15:44:49 +0700

    add feature update user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/user.dto';
 import { PrismaService } from '../../prisma/prisma.service';
 import { User } from './entities/user.entity';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Injectable()
 export class UserService {
@@ -32,4 +33,21 @@ export class UserService {
     async getUserById(userId: number) {
         return this.prisma.user.findUnique({ where: { id: userId } })
     }
+
+    async updateUser(updateUserDto: UpdateUserDto) {
+        const existingUser = await this.prisma.user.findUnique({
+            where: { id: updateUserDto.id }
+        });
+
+        if (!existingUser) {
+            throw new NotFoundException(`User with ID ${updateUserDto.id} not found`);
+        }
+
+        // Update the user with the new data
+        return this.prisma.user.update({
+            where: { id: updateUserDto.id },
+            data: updateUserDto,
+        });
+    }
 }
+
