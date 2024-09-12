6b751e7023df13c50ee12e5d0f1e53faeda2d06b
HoaiBaoNguyen <hoaibao2129@gmail.com>
2024-09-12 15:58:39 +0700

    add feature authorization for app

---
+++ src/helper/role.guard.ts
@@ -0,0 +1,56 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+import { PrismaService } from 'prisma/prisma.service';
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private prisma: PrismaService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requestToken = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(requestToken);
+    const requiredRoles = this.reflector.get<string>(
+      'roles',
+      context.getHandler(),
+    );
+    if (!requiredRoles) {
+      return true;
+    }
+    const userGet = await this.prisma.user.findMany({
+      where: { token: token },
+    });
+    if (userGet.length === 0) {
+      throw new ForbiddenException('User does not exist');
+    }
+    const roleGet = await this.prisma.role.findMany({
+      where: { id: userGet[0].roleId },
+    });
+    console.log(roleGet);
+    if (!roleGet) {
+      throw new ForbiddenException('Roles does not exist');
+    }
+    if (roleGet[0].roleName !== requiredRoles) {
+      throw new ForbiddenException(
+        'You do not have permission to access this resource',
+      );
+    }
+    return true;
+  }
+
+  private hasRole(user: any, roles: string[]): boolean {
+    return roles.some((role) => user.roles?.includes(role));
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -10,12 +10,17 @@ import { ProductService } from './product.service';
 import { isEmpty } from 'src/helper/checkEmptyData';
 import { CreateProductDto } from './dto/create-product.dto';
 import { AuthGuard } from 'src/helper/auth.guard';
+import { Roles } from 'src/decorator/roles.decorator';
+import { Role } from 'src/enum/role.enum';
+import { RolesGuard } from 'src/helper/role.guard';
 
 @Controller('product')
 @UseGuards(AuthGuard)
 export class ProductController {
   constructor(private productService: ProductService) {}
   @Post('')
+  @Roles(Role.Admin)
+  @UseGuards(RolesGuard)
   async createProduct(@Body() createProductDto: CreateProductDto) {
     if (isEmpty(createProductDto)) {
       throw new BadRequestException('User data can not be empty');
